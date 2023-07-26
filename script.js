fc4a2191802db9f9f8d0e9b779155d3a7973d45a
aadildil <117893260+aadildil@users.noreply.github.com>
2023-07-26 17:17:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-//your JS code here. If required.
 const div=document.getElementById("browser-info");
-const ino=`You are using ${navigator.userAgent}`;
+const str=navigator.userAgent;
+let ele=str.split(" ");
+let ans2=ele.slice(1);
+
+let browser=ele[0].split("/")[0];
+let version=ele[0].split("/")[1];
+
 
  const para=document.createElement("p");
- para.innerText=ino;
+ para.innerText=`You are using ${browser} version ${version} ${ans2}`;
 div.appendChild(para);
