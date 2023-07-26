d0748e17b0b1530514f534874b27c8dfee41fb78
aadildil <117893260+aadildil@users.noreply.github.com>
2023-07-26 15:56:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const div=document.getElementById("browser-info");
+const ino=`You are using ${navigator.userAgent}`;
+
+ const para=document.createElement("p");
+ para.innerText=ino;
+div.appendChild(para);
